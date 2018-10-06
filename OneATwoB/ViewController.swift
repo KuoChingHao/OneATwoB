acba1fcccb9f1b66f1b65608c0e6ea32a976d9e8
Tank <zxm55547@gmail.com>
2018-10-06 14:31:35 +0800

    Finish

--- OneATwoB/ViewController.swift
+++ OneATwoB/ViewController.swift
@@ -15,14 +15,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayResultLabel: UILabel!
     
+    var resultString = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        resultString = createResultNumberString()
+        
     }
 
     
     @IBAction func numberBtnAction(_ sender: Any) {
+        
+        guard let currentContentText = displayEnterNumberTextField.text else { return }
+        
+        let numberString = (sender as! UIButton).titleLabel?.text
+        
+        displayEnterNumberTextField.text = enterNumber(currentContentText: currentContentText, enter: numberString!)
         
     }
     
@@ -30,12 +41,85 @@ class ViewController: UIViewController {
     @IBAction func confirmBtnAction(_ sender: Any) {
         
         
+        guard let userEnterNumberString = displayEnterNumberTextField.text , userEnterNumberString.count == 4 else {
+            return
+        }
+        
+        
+         displayResultLabel.text = checkResult(userEnterNumberString: userEnterNumberString, resultNumberString: resultString)
+        
+    }
+    func checkResult(userEnterNumberString : String , resultNumberString : String) -> String{
+        
+        
+        
+        var bResult = 0
+        var aResult = 0
+        
+        for (userIndex,userItem) in userEnterNumberString.enumerated() {
+
+            if resultNumberString.contains(userItem) {
+                bResult += 1
+                
+                for (resultIndex,resultItem) in resultNumberString.enumerated(){
+                    if userIndex == resultIndex && userItem == resultItem {
+                        aResult += 1
+                        bResult -= 1
+                    }
+                }
+            }
+        }
+        
+        var resultString = ""
+        
+        if aResult > 0 {
+            resultString = "\(aResult)A"
+        }
+    
+        if bResult >= 0 && aResult != 4 {
+            resultString += "\(bResult)B"
+        }
+        
+        return resultString
     }
     
     
     
     
-    
+    func enterNumber(currentContentText : String ,enter : String) -> String{
+        var result = currentContentText
 
+        if currentContentText.contains(enter) == false && currentContentText.count < 4 {
+            result += enter
+        }
+        
+        return result
+    }
+    
+    
+    
+    func createResultNumberString() -> String{
+        
+        var numberArray = [Int]()
+        
+        for _ in 1...4 {
+            
+            var random = arc4random() % 10
+            
+            while numberArray.contains(Int(random)) {
+                random = arc4random() % 10
+            }
+            
+            numberArray.append(Int(random))
+            
+        }
+        
+        var resultString = ""
+        
+        numberArray.forEach { (i) in
+            resultString += String(i)
+        }
+        return resultString
+    }
 }
 
--- OneATwoBTests/OneATwoBTests.swift
+++ OneATwoBTests/OneATwoBTests.swift
@@ -11,8 +11,13 @@ import XCTest
 
 class OneATwoBTests: XCTestCase {
 
+    
+    var vc : ViewController!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        vc = storyboard.instantiateInitialViewController() as? ViewController
     }
 
     override func tearDown() {
@@ -31,5 +36,73 @@ class OneATwoBTests: XCTestCase {
         }
     }
     
+    
+    func testCreateResultNumber(){
+        
+        let numbers = vc.createResultNumberString()
+        if numbers.count == 4 {
+            XCTAssert(true, "\(numbers)")
+        }else {
+            XCTAssert(false, "產生數字錯誤")
+        }
+    }
+    
+    func testCheckEnterNumber1(){
+        
+        let currentContentText = "123"
+        
+        let resultString = vc.enterNumber(currentContentText: currentContentText, enter: "1")
+        
+        if resultString == "123" {
+            XCTAssert(true)
+        }else {
+            XCTAssert(false)
+        }
+        
+        
+        
+    }
+
+    func testCheckEnterNumber2(){
+        
+        let currentContentText = "123"
+        
+        let resultString = vc.enterNumber(currentContentText: currentContentText, enter: "4")
+        
+        if resultString == "1234" {
+            XCTAssert(true)
+        }else {
+            XCTAssert(false)
+        }
+    }
+    
+    func testCheckEnterNumber3(){
+        
+        let currentContentText = "1234"
+        
+        let resultString = vc.enterNumber(currentContentText: currentContentText, enter: "5")
+        
+        if resultString == "1234" {
+            XCTAssert(true)
+        }else {
+            XCTAssert(false)
+        }
+    }
+    
+    
+    func testCheckResult(){
+        
+        let userEnterNumberString = "1234"
+        let resultString = "1234"
+        let result = vc.checkResult(userEnterNumberString: userEnterNumberString, resultNumberString: resultString)
+        
+        if result == "4A"{
+            XCTAssert(true,result)
+        }else {
+            XCTAssert(false,result)
+        }
+        
+        
+    }
     
 }
